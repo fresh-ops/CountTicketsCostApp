0bf8b4528e87def6d2cedf9c47c8a0a2d0a73148
Fresh <solovev-matveyka@inbox.ru>
2023-02-01 21:42:12 +0700

    Documented TicketCart class

--- app/src/main/java/com/fresh/countticketscostapp/tickites/TicketCart.java
+++ app/src/main/java/com/fresh/countticketscostapp/tickites/TicketCart.java
@@ -3,13 +3,25 @@ package com.fresh.countticketscostapp.tickites;
 import java.util.ArrayList;
 import java.util.Objects;
 
+/**
+ * Класс корзины билетов
+ */
 public class TicketCart {
+    /** Поле списка билетов */
     private ArrayList<BusTicket> tickets = new ArrayList<BusTicket>();
 
+    /**
+     * Метод добавления билета в корзину
+     * @param ticket сущность билета для добавления
+     */
     public void addTicket(BusTicket ticket) {
         tickets.add(ticket);
     }
 
+    /**
+     * Метод подсчёта общей стоимости билетов в корзине
+     * @return стоимость билетов
+     */
     public float countCost() {
         float cost = 0;
 
@@ -20,6 +32,11 @@ public class TicketCart {
         return cost;
     }
 
+    /**
+     * Метод поиска билетов в корзине
+     * @param type тип билета
+     * @return экземпляр TicketCart, содержащий билеты указанного типа
+     */
     public TicketCart findByTicketType(String type) {
         TicketCart found = new TicketCart();
 
